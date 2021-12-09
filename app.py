7e3c82e8859e7bb6615cb156d7f5980c47b78a00
GT2017 <63583056+GT2017@users.noreply.github.com>
2021-12-09 17:08:40 +0100

    First Vader Version

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import redis
 from flask import Flask
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 cache = redis.Redis(host = 'redis', port = 6379)
+
+def vader_analyse(text):
+    Analyzeranalyser = SentimentIntensityAnalyzer()
+    score = sentiment_analyzer_scores(text)
+    return score
 
 @app.route('/')
 def index():
@@ -14,6 +20,6 @@ def index():
 
                 <script>
                 function getInfo() {
-                    document.getElementById("result").innerHTML = document.getElementById("text").value;
+                    document.getElementById("result").innerHTML = vader_analyse(document.getElementById("text").value);
                 }
                 </script>'''
