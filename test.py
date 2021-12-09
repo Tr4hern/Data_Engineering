4b8efc357a17c5ff1df8f8dc04c74380b690c9da
Trahern <spammenotnameho@gmail.com>
2021-12-09 21:02:59 +0100

    vader tests

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 import pytest
 import requests
+import app
 
 def test_status():
     assert requests.get("http://localhost:5000").status_code == 200
+
+def test_sentiment():
+    assert app.vader_analyse("I love cake") == "Positive"
+    assert app.vader_analyse("There is a cat") == "Neutral"
+    assert app.vader_analyse("I hate this project") == "Negative"
+
+def test_accuracy():
+    assert app.get_accuracy() >= 0.8
