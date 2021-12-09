5d49fa3380d3d4a8823856c1e9ec1043ac8187cf
Trahern <spammenotnameho@gmail.com>
2021-12-09 20:57:54 +0100

    Revert "Merge branch 'Vader' into test"
    This reverts commit 48409bf8955a426d1c3a644a338673499d2e15b1.

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
 import redis
 from flask import Flask
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 cache = redis.Redis(host = 'redis', port = 6379)
 
-def vader_analyse(text):
-    analyzeranalyser = SentimentIntensityAnalyzer()
-    score = analyser.polarity_scores(text)
-    return "{:-<40} {}".format(text, str(score))
 
 @app.route('/')
-def index():
-    return  '''
-                <label for="text">Text Input:</label><br>
-                <input type="text" id="text" name="text" value=""><br>
-                <button type="button" onclick="getInfo()"> Analyse </button><br>
-                <p id = "result"></p><br>
-
-                <script>
-                function getInfo() {
-                    document.getElementById("result").innerHTML = vader_analyse(document.getElementById("text").value);
-                }
-                </script>'''
+def main():
+    return "Test webapp"
