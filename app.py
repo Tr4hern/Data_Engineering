4515af848b2c89c2ef6646cfabbb0aeb99772a5f
Zelogia <nicolas.d.kourlukoff@mail.com>
2021-12-09 15:23:30 +0100

    First version web interface

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ cache = redis.Redis(host = 'redis', port = 6379)
 
 
 @app.route('/')
-def main():
-    return "Test webapp"
+def index():
+    return '<form action="/action_page.php"><label for="text">Text Input:</label><br><input type="text" id="text" name="text" value=""><br><input type="submit" value="Submit"></form>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
