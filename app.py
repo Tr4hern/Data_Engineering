358d300f8597ef7a1d5b418eccf91652b43dcd01
Zelogia <nicolas.d.kourlukoff@mail.com>
2021-12-09 15:57:29 +0100

    version propre

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ cache = redis.Redis(host = 'redis', port = 6379)
 
 @app.route('/')
 def index():
-    return '<form action="/action_page.php"><label for="text">Text Input:</label><br><input type="text" id="text" name="text" value=""><br><input type="submit" value="Submit"></form>'
+    label = '<label for="text">Text Input:</label>'
+    text = '<input type="text" id="text" name="text" value="">'
+    button = "<button type=\"button\"> Button </button>"
+    br = '<br>'
+
+    return '<form action="/action_page.php">' + label + br + text + br + button +'</form>'
+    #return '<form action="/action_page.php"><label for="text">Text Input:</label><br><input type="text" id="text" name="text" value=""><br><br><button onclick="doSomething()" type="button"> Button </button><p id="test">Hello</p><script>function doSomething(){document.getElementById("test).innerHTML = "Good Bye ";</script>}</form>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
