adb9247a52813a08acce0ec5bfc0af2e771bf62f
Zelogia <37299631+Zelogia@users.noreply.github.com>
2022-02-28 14:39:15 +0100

    detoxify

---
+++ work/toxicity.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from detoxify import Detoxify
+import json
+
+def detoxify_analyse(text):
+
+    results = Detoxify('multilingual').predict(text)
+
+    return results
+
+
+def detoxify_json_to_string(json_file):
+    return json.dumps(json_file)
+
