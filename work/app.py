2f1d24d365e68b68f62dc081a984de7eb1337826
Trahern <spammenotnameho@gmail.com>
2022-02-28 17:05:15 +0100

    beautiful results

--- work/app.py
+++ work/app.py
@@ -5,14 +5,22 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 cache = redis.Redis(host = 'redis', port = 6379)
 
+def beautitful_text(text):
+    text = text.replace('\'', '')
+    text = text.replace('\"', '')
+    text = text.replace('{', '')
+    text = text.replace('}', '')
+    text = text.replace(',', '<br>')
+    return text
+
+
 @app.route('/')
 def index():
     return  render_template('index.html')
 
+
 @app.route('/sentiment', methods=['GET'])
 def sentiment():
-    print("fetch")
     result = toxicity.detoxify_json_to_string(toxicity.detoxify_analyse(request.args.get("message")))
-    print(result)
+    result = beautitful_text(result)
     return jsonify("{}".format(result))
-
