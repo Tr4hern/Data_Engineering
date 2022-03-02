074b067d2870be3275bdbc045f9277c1f742ec8d
Trahern <spammenotnameho@gmail.com>
2022-03-02 17:37:29 +0100

    stress test functionnal

--- work/functionnal_tests.py
+++ work/functionnal_tests.py
@@ -3,16 +3,16 @@ warnings.filterwarnings('ignore')
 import requests
 import time
 
-def get_pinged():
+def get_pinged(url, nb_ping):
     start_time = time.time()
-    urls = ["http://localhost:5000"] * 100
+    urls = [url] * nb_ping
     for i in urls:
         requests.get(i)
-    return ((time.time() - start_time) / 100)
+    return ((time.time() - start_time) / nb_ping)
 
 
 def test_status():
     assert requests.get("http://localhost:5000").status_code == 200
 
-def stress_test():
-    assert get_pinged() <= 0.1
+def test_stresstest():
+    assert get_pinged("http://localhost:5000", 100) <= 3
