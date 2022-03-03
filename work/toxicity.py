db8485c5a37405fd8aee2f510e951f194ca90548
Trahern <spammenotnameho@gmail.com>
2022-03-03 23:52:45 +0100

    better detoxify analysis

--- work/toxicity.py
+++ work/toxicity.py
@@ -1,14 +1,21 @@
-import pandas as pd
 from detoxify import Detoxify
 import json
 
+from torch import det
+
 def detoxify_analyse(text):
 
-    results = Detoxify('multilingual').predict(text)
+    results = Detoxify('original').predict(text)
+    result = {}
+    for i in results.keys():
+        if(results[i] >= 0.5):
+            result[i] = results[i]
+    
+    if bool(result) is False:
+        result['Toxicity Detector'] = text + " is not toxic"
 
-    return results
+    return result
 
 
 def detoxify_json_to_string(json_file):
     return json.dumps(str(json_file))
-
--- work/unit_tests.py
+++ work/unit_tests.py
@@ -2,8 +2,27 @@ import warnings
 warnings.filterwarnings('ignore')
 import toxicity
 
+def check_threshold():
+    result = toxicity.detoxify_analyse("bitch")
+    for i in result.keys():
+        if result[i] < 0.5:
+            return False
+    return True
+
+def english():
+    result = toxicity.detoxify_analyse("connard")
+    return result['Toxicity Detector']
+
+def not_toxic():
+    result = toxicity.detoxify_analyse("it's a beautiful day")
+    return result['Toxicity Detector']
+
+
 def test_toxicity_json():
-    assert len(toxicity.detoxify_analyse("test")) == 7
+    assert len(toxicity.detoxify_analyse("test")) <= 7
+    assert check_threshold() == True
+    assert isinstance(english(), str)
+    assert isinstance(not_toxic(), str)
 
 
 def test_json_to_text():
